f55254d36d4423dd13879fbea35d95b3fa650dd3
harsh-1503 <harshkarwa1345@gmail.com>
2022-09-07 23:46:32 +0530

    3rd Commit

--- main.cpp
+++ main.cpp
@@ -152,9 +152,158 @@ public:
 
             default:
                 cout<<"\t\t\t Wrong choice ! Please try again "<<endl;
-
         }
     }
+};
+
+class student{
+    public:
+    void Studentpage()
+    {
+        system("cls");
+        int r;
+        cout << "\t\t\t--------------------------------------------------------------------\n\n";
+        cout << "\t\t\t\t\t            STUDENT MENU                                        \n\n";
+        cout << "\t\t\t--------------------------------------------------------------------\n\n";
+        cout << "\t| Press 1 for STUDENT LOGIN        |\n";
+        cout << "\t| Press 2 for STUDENT REGISTRATION |\n";
+        cout << "\t| Press 3 for FORGOT  PASSWORD     |\n\n";
+        cout << "Enter your choice --> ";
+        cin >> r;
+
+        switch (r)
+        {
+        case 1:
+            loginStudent();
+            break;
+        case 2:
+            registerStudent();
+            break;
+        default:
+            forgot();
+            break;
+        }
+    }
+    void loginStudent()
+    {
+        int cnt = 0;
+        int c;
+        string userIDR, password, idR, passR;
+        system("cls");
+        cout << "\n\n\t\t Please enter your Credentials : \n";
+        cout << "\t\t USERNAME --> ";
+        cin >> userIDR;
+        cout << "\t\t PASSWORD --> ";
+        c = getch();
+        while (c != 13)
+        {
+            password.push_back(c);
+            cout << "*";
+            c = getch();
+        }
+
+        ifstream rectorcreds("students.txt");
+
+        while (rectorcreds >> idR >> passR)
+        {
+            if (idR == userIDR and passR == password)
+            {
+                cnt++;
+                system("cls");
+            }
+        }
+        rectorcreds.close();
+
+        if (cnt)
+        {
+            cout << "\t" << userIDR << "\n\tYour LOGIN is successfull!";
+        }
+        else
+        {
+            cout << "\n\t Account NOT FOUND\n\t Please enter correct Credentials\n";
+        }
+        // firstpage();
+    }
+    void registerStudent()
+    {
+        string Suserid, Spassword, Sid, Spass;
+        int c;
+        system("cls");
+        cout << "\t\t USERNAME --> ";
+        cin >> Suserid;
+        cout << "\t\t PASSWORD --> ";
+        c = getch();
+        while (c != 13)
+        {
+            Spassword.push_back(c);
+            cout << "*";
+            c = getch();
+        }
+        ofstream rectorcreds("students.txt", ios::app);
+        rectorcreds << "\n"
+                    << Suserid << " " << Spassword;
+        system("cls");
+        cout << "\n\n\t\t Registration was successfull!! \n";
+        rectorcreds.close();
+        Studentpage();
+    }
+
+    void forgot()
+    {
+        int option;
+        system("cls");
+        cout<<"\t\t\tForgot password? Chill! We are here\n\n";
+        cout<<"| Press 1 to search your id by username |"<<endl;
+        cout<<"| Press 2 to go back to the main menu   |\n "<<endl;
+        cout<<"  Enter your choice -->";
+        cin >> option;
+
+        switch(option)
+        {
+            case 1:
+            {
+                int count = 0;
+                string SuserId,SId,Spass;
+                cout<<"\n\t\t\tEnter the username you remember: ";
+                cin >> SuserId;
+
+                ifstream Studentcreds("students.txt",ios::app);
+                while(Studentcreds>>SId>>Spass)
+                {
+                    if(SId  == SuserId)
+                    {
+                        count = 1;
+                        break;
+                    }
+                }
+                
+                Studentcreds.close();
+                
+                if(count == 1)
+                {
+                    cout<<"\n\n Your account is found! \n";
+                    cout<<"\n\n Your password is : "<<Spass;
+                    getch();
+                    main();
+                }
+                else{
+                    cout<<"\n\t Sorry! Your account is not found: \n";
+                    getch();
+                    main();
+                }
+                break;
+            }
+
+            case 2:
+            {
+                main();
+            }
+
+            default:
+                cout<<"\t\t\t Wrong choice ! Please try again "<<endl;
+        }
+    }
+
 };
 
 int main()
@@ -176,6 +325,10 @@ int main()
         rector R;
         R.firstpage();
         break;
+
+    case 3:
+        student S;
+        S.Studentpage();
 
     default:
         break;
